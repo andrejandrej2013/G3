34b2c171a878f7a781390568f60066473e1b99c4
Andrejs Grinevics <grinevic67@gmail.com>
2022-02-16 11:54:28 +0200

    Добавьте файлы проекта.

---
+++ G3/G3.cpp
@@ -0,0 +1,22 @@
+﻿/*******************************************
+Andrejs Griņevičs, ag21199
+G3. Create function to delete all elements in list with value less than value of previous element.
+Program created: 2022/02/11
+*******************************************/
+
+#include <iostream>
+
+using namespace std;
+
+struct Node_list {
+    int val;
+    Node_list* next;
+    Node_list(int val) : val(val), next(nullptr) {}
+    Node_list(int val, Node_list* next) : val(val), next(next) {}
+};
+
+int main()
+{
+
+    return 0;
+}
